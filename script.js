30febe1b004e26598fd51cbc1b6f4625d0995838
RomanRomaan <jay4or@gmail.com>
2025-07-08 15:49:09 +0300

    final

---
+++ script.js
@@ -0,0 +1,14 @@
+// Вивести таблицю Піфагора (10×10), таблиця повинна бути створена динамічно
+const table = document.createElement('table');
+
+for (i = 0; i < 10; i++) {
+    let tr = document.createElement('tr');
+    table.appendChild(tr)
+    for (n = 0; n < 10; n++) {
+        let td = document.createElement('td');
+        td.textContent = (i + 1) * (n + 1);
+        tr.appendChild(td)
+    }
+}
+table.classList.add('pifagor-table');
+document.body.appendChild(table);
